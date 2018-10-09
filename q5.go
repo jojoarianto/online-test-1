0c56e6f505e67fa40d88df1e341a45ded5a6dac1
Joko Irianto <irianto94@outlook.com>
2018-10-09 22:35:01 +0700

    progress q5

--- q5.go
+++ q5.go
@@ -22,6 +22,11 @@ func main() {
 			words[i] = make([]string, m)
 		}
 
+		var army []string
+		var armyLocX []int
+		var armyLocY []int
+		// var armyQty []int
+
 		/**
 		 * I use 2d array to represent the map
 		 *
@@ -35,6 +40,8 @@ func main() {
 		 * value: 0 = not visited node
 		 * 		  1 = mountain
 		 * 		  2 = visited node
+		 *
+		 * Stack status :
 		 */
 		for i := 0; i < n; i++ {
 			var str string
@@ -45,14 +52,33 @@ func main() {
 				words[i][idx] = dt
 
 				// find army
-				if "#" != dt || "." != dt {
-
+				if "#" != dt && "." != dt {
+					army = append(army, dt)
+					armyLocX = append(armyLocX, idx)
+					armyLocY = append(armyLocY, i)
 				}
 			}
 		}
 
+		fmt.Println(army)
+		fmt.Println(armyLocX)
+		fmt.Println(armyLocY)
+
 		// find army
 
+	}
+
+	/**
+	 * using depth search algorithm
+	 *
+	 * move to down
+	 * move to right
+	 * move to up
+	 * move to left
+	 */
+	func travelFindAnotherArmy(army string, armyLocX int, armyLocY) {
+		
+		
 	}
 
 }
